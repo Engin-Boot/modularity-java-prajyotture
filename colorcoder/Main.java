bcc324f2d60a99515403eb31f6dec59a1d500e3b
Prajyot Ture <47532830+prajyotture@users.noreply.github.com>
2020-09-06 16:03:27 +0530

    Add files via upload

---
+++ colorcoder/Main.java
@@ -0,0 +1,15 @@
+package colorcoder;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+			ColorUtil.testNumberToPair(4, MajorColor.WHITE, MinorColor.BROWN);
+			ColorUtil.testNumberToPair(5, MajorColor.WHITE, MinorColor.SLATE);
+
+			ColorUtil.testPairToNumber(MajorColor.BLACK, MinorColor.ORANGE, 12);
+			ColorUtil.testPairToNumber(MajorColor.VIOLET, MinorColor.SLATE, 25);
+			
+	}
+
+}
---
+++ colorcoder/MainClient.java
@@ -0,0 +1,13 @@
+package colorcoder;
+
+public class MainClient {
+
+	public static void main(String[] args) {
+		ColorUtil.testNumberToPair(4, MajorColor.WHITE, MinorColor.BROWN);
+		ColorUtil.testNumberToPair(5, MajorColor.WHITE, MinorColor.SLATE);
+
+		ColorUtil.testPairToNumber(MajorColor.BLACK, MinorColor.ORANGE, 12);
+		ColorUtil.testPairToNumber(MajorColor.VIOLET, MinorColor.SLATE, 25);
+		
+	}
+}
